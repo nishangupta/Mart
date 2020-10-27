6a2fa9e6aaddcdaa84dbe881ccfaa4a0d5075f20
nisangupta <nishangupta@icloud.com>
2020-10-27 19:06:01 +0545

    adding sweetalert and spatie permissions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
                 font-family: 'Nunito';
             }
         </style>
+        @include('sweetalert::alert')
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
