7cb4158f02b295e0a7d8ff7df36625897315f934
nisangupta <nishangupta@icloud.com>
2020-11-06 19:29:15 +0545

    final touch

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,12 +5,11 @@
   <!-- Page Heading -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    <button onclick="window.print()" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</button>
   </div>
 
   <!-- Content Row -->
   <div class="row">
-
     
     <div class="col-xl-3 col-md-6 mb-4">
       <a href="{{route('product.index')}}" style="text-decoration: none"> 
@@ -83,10 +82,6 @@
         </div>
       </a>
     </div>
-
-    
-
-
 
   </div>
 
--- resources/views/inc/admin/topbar.blade.php
+++ resources/views/inc/admin/topbar.blade.php
@@ -7,11 +7,11 @@
   </button>
   
   <!-- Topbar Search -->
-  <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" action="route('home.search')}}">
+  <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" action="">
     <div class="input-group">
       <input name="q" type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
       <div class="input-group-append">
-        <button type="submit" class="btn btn-primary" type="button">
+        <button type="submit" class="btn btn-primary" >
           <i class="fas fa-search fa-sm"></i>
         </button>
       </div>
