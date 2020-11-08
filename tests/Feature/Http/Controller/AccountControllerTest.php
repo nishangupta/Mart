9bad912344b06cecfdb25613d5777c103b13fe7c
nisangupta <nishangupta@icloud.com>
2020-11-08 09:53:25 +0545

    AccountControllerTest completed

---
+++ tests/Feature/Http/Controller/AccountControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class AccountControllerTest extends TestCase
+{
+  use RefreshDatabase, WithFaker;
+
+  /** @test */
+  public function userCanLogout()
+  {
+    $response = $this->get('/logout');
+    $response->assertRedirect('/login');
+  }
+
+  /** @test */
+  public function NonAuthenticatedUserCannotChangePassword()
+  {
+    $response = $this->from('/user')->put('/account/changePassword', [
+      'current_password' => 'asdasdasd',
+      'new_password' => 'asdasdasdasd',
+      'confirmation_password' => 'asdasdasdasd'
+    ]);
+
+    $response->assertRedirect('/user');
+    $this->assertGuest();
+  }
+
+  /** @test */
+  public function userCanChangePassword()
+  {
+    Artisan::call('db:seed', ['--class' => 'RoleSeeder']);
+    User::factory(1)->create();
+    $user = User::first();
+    $oldp = $user->password;
+    $this->be($user);
+
+    $response = $this->put('/account/changePassword', [
+      'current_password' => 'password',
+      'new_password' => 'newpassword',
+      'confirm_password' => 'newpassword'
+    ]);
+
+    $this->assertTrue(Hash::check('newpassword', User::first()->password));
+  }
+}
