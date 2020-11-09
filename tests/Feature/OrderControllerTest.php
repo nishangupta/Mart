0f512a6655784830d04dd60a42956ecce6c9231d
nisangupta <nishangupta@icloud.com>
2020-11-09 08:24:31 +0545

    OrderControllerTest

---
+++ tests/Feature/OrderControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Order;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class OrderControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function userCanPlaceOrder()
+    {
+        Artisan::call('db:seed');
+        Auth::loginUsingId(2); //default user from user seeder
+
+        $response = $this->post('/direct-buy', [
+            'id' => 1
+        ]);
+
+        $this->assertCount(1, Order::all());
+    }
+}
