0df0d81eac2ff0b17d9c80bb8dc669018a26bdb6
MilesChou <jangconan@gmail.com>
2023-07-07 16:52:40 +0800

    Add FeatureFlags provider

---
+++ app/Providers/FeatureFlagsServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Pennant\Feature;
+
+class FeatureFlagsServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+    }
+}
--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\FeatureFlagsServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
